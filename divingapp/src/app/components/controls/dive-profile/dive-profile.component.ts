484f89feec5a417f4b59e72e9461a9590a6fb70c
blackshadev <vinnie@script4web.nl>
2017-05-01 19:53:55 +0200

    Added crosshair in profile

--- divingapp/src/app/components/controls/dive-profile/dive-profile.component.ts
+++ divingapp/src/app/components/controls/dive-profile/dive-profile.component.ts
@@ -1,7 +1,9 @@
+import { debounce } from '../../../shared/common';
 import { Dive, TSample } from '../../../shared/dive';
 import { DiveStore } from '../../../services/dive.service';
 import { Component, OnInit, Input, ElementRef, ViewChild, AfterViewInit, HostListener } from '@angular/core';
 import * as d3 from 'd3';
+
 
 @Component({
   selector: 'app-dive-profile',
@@ -20,10 +22,12 @@ export class DiveProfileComponent implements OnInit, AfterViewInit {
   private svg: d3.Selection<any, any, null, undefined>;
   private groups: {
     line: d3.Selection<any, any, null, undefined>,
-    axis: d3.Selection<any, any, null, undefined>,
+    leftAxis: d3.Selection<any, any, null, undefined>,
+    topAxis: d3.Selection<any, any, null, undefined>,
+    focus: d3.Selection<any, any, null, undefined>,
   };
   private _margin = {
-    left: 20,
+    left: 60,
     right: 20,
     top: 20,
     bottom: 20,
@@ -38,6 +42,11 @@ export class DiveProfileComponent implements OnInit, AfterViewInit {
   };
   private _line: d3.Line<TSample>;
   private _data: TSample[] = [];
+  private _axes: {
+    left: d3.Axis<number|{valueOf(): number}>,
+    top: d3.Axis<number|{valueOf(): number}>,
+  };
+  private bisect = d3.bisector<TSample, number>((d: TSample) => d.Time).left;
 
   constructor(
     private service: DiveStore,
@@ -48,28 +57,69 @@ export class DiveProfileComponent implements OnInit, AfterViewInit {
       y: d3.scaleLinear(),
     };
     this._line = d3.line<TSample>()
-      .curve(d3.curveBasis)
+      .curve(d3.curveCardinal)
       .x((d) => this._scale.x(d.Time))
       .y((d) => this._scale.y(d.Depth));
     this.svg = d3.select(this.elRef.nativeElement)
       .append('svg');
     this.groups = {
       line: this.svg.append('g').attr('class', 'group-line'),
-      axis: this.svg.append('g').attr('class', 'group-axis'),
+      leftAxis: this.svg.append('g').attr('class', 'group-left-axis'),
+      topAxis: this.svg.append('g').attr('class', 'group-top-axis'),
+      focus: this.svg.append('g').attr('class', 'group-focus'),
     };
-    this.groups.line.attr('transform', `translate(${this._margin.left},${this._margin.top})`)
+    this.groups.line.attr('transform', `translate(${this._margin.left},${this._margin.top})`);
+    this._axes = {
+      left: d3.axisLeft(this._scale.y),
+      top: d3.axisTop(this._scale.x),
+    };
+
+    this.groups.focus.append('line')
+      .attr('class', 'x')
+      .attr('y1', this._margin.top).attr('y2', 0);
+    this.groups.focus.append('line')
+      .attr('class', 'y')
+      .attr('x1', this._margin.left).attr('x2', 0);
+
+    this.svg
+    .on('mouseover', () => { this.groups.focus.style('display', null); })
+      .on('mouseout', () => { this.groups.focus.style('display', 'none'); })
+      .on('mousemove', () => {
+        const mouse = d3.mouse(this.svg.node());
+        if (mouse) {
+          this.select(mouse[0]);
+        }
+      });
   }
 
   ngAfterViewInit(): void {
     this.groups.line.append('path')
-      .attr('class', 'line')
-      .style('fill', 'none')
-      .style('stroke', 'black');
+      .attr('class', 'line');
     this.resize();
   }
 
   ngOnInit() {
     this.paint()
+  }
+
+  private select(mouseX: number) {
+    const mouseTime = this._scale.x.invert(mouseX - this._margin.left);
+    const iX = this.bisect(this._data, mouseTime);
+    const item = this._data[iX];
+    const d0 = this._data[iX - 1]
+    const d1 = this._data[iX];
+    // work out which date value is closest to the mouse
+    const d = mouseTime - d0[0] > d1[0] - mouseTime ? d1 : d0;
+    const focusCrosshair = {
+      x: this.groups.focus.select('line.x'),
+      y: this.groups.focus.select('line.y'),
+    }
+    const pos = {
+      x: this._scale.x(d.Time) + this._margin.left,
+      y: this._scale.y(d.Depth) + this._margin.top,
+    }
+    focusCrosshair.x.attr(`x1`, pos.x).attr(`x2`, pos.x);
+    focusCrosshair.y.attr(`y1`, pos.y).attr(`y2`, pos.y);
   }
 
   private async getSamples() {
@@ -93,6 +143,7 @@ export class DiveProfileComponent implements OnInit, AfterViewInit {
   }
 
   public paint() {
+    this.repaintAxes();
     const all = this.groups.line.selectAll('path');
     all.data([this._data]);
 
@@ -101,7 +152,7 @@ export class DiveProfileComponent implements OnInit, AfterViewInit {
     all.transition()
       .ease(d3.easeLinear)
       .duration(1000)
-      .attr('d', this._line(this._data))
+      .attr('d', this._line(this._data));
 
     all.exit().remove();
   }
@@ -115,9 +166,22 @@ export class DiveProfileComponent implements OnInit, AfterViewInit {
     this.svg.attr('height', height);
     this._boundingbox.width = width - this._margin.left - this._margin.right;
     this._boundingbox.height = height - this._margin.top - this._margin.bottom;
+
+    this.groups.focus.select('line.x').attr('y2', height);
+    this.groups.focus.select('line.y').attr('x2', width);
+
     this._scale.x.range([0, this._boundingbox.width]);
     this._scale.y.range([0, this._boundingbox.height]);
     this.paint();
+  }
+
+  protected repaintAxes() {
+    this.groups.leftAxis.attr('transform', 'translate(50, 20)')
+    const all = this.groups.leftAxis.selectAll();
+    all.remove();
+    this.groups.leftAxis.call(
+      this._axes.left
+    );
   }
 
 }

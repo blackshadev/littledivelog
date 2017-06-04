60a636ef93b9a3d79e869021057f9558f781ef64
Vincent Hagen <vinnie@script4web.nl>
2017-06-04 13:59:25 +0200

    working on db reset script

---
+++ divingserver/db/1_tables/01_users.sql
@@ -0,0 +1,11 @@
+
+create table if not exists users (
+    user_id         serial                                                      not null
+  , email           text                                                        not null
+  , password        text                                                        not null
+  , salt            text                                                        not null
+  , name            text                                                        not null
+
+  , inserted        timestamp   default (current_timestamp at time zone 'UTC')  not null
+  , primary key(user_id)
+);

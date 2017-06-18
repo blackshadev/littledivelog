cb317f75738ff27c8dc6957c0e5bf0df415d4b36
Vincent Hagen <vinnie@script4web.nl>
2017-06-18 17:11:59 +0200

    added debug statement

--- divingserver/src/import.ts
+++ divingserver/src/import.ts
@@ -36,6 +36,7 @@ interface IComputerImport {
 
 router.get("/", async (req, res) => {
 
+    console.log(req.user.user_id);
     const user: QueryResult = await database.call(
         `select
               usr.user_id

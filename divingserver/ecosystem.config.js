adae497577eb9108dbda396260859d4422ba3d85
blackshadev <vinnie@script4web.nl>
2017-09-07 19:35:14 +0200

    Added ecosystem.config.js

---
+++ divingserver/ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'App Server',
+      cwd       : "./diveserver/",
+      script    : 'build/app.js',
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    prod : {
+      key: "~/.ssh/dive_app_key",
+      user : 'dive.littledev.nl',
+      host : 'mira.littledev.nl',
+      ref  : 'origin/master',
+      repo : 'git@git.littledev.nl:blackshade/divecomputer.git',
+      path : '/home/dive.littledev.nl/',
+      'post-deploy' : 'cd diveserver && npm install && tsc && pm2 reload ecosystem.config.js'
+    }
+  }
+};

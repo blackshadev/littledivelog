0ecada6f3d4fecbceaa435823c9bad7100df48f0
blackshadev <vinnie@script4web.nl>
2017-09-07 19:36:00 +0200

    Fixed cwd

--- divingserver/ecosystem.config.js
+++ divingserver/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // First application
     {
       name      : 'App Server',
-      cwd       : "./diveserver/",
+      cwd       : "./divingserver/",
       script    : 'build/app.js',
     }
   ],

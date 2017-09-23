055730f5732890dd752a550bde397942458d9b06
blackshadev <vinnie@script4web.nl>
2017-09-23 20:20:05 +0200

    working on request helper function, and computer post call

---
+++ DiveLogUploader/Request.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace DiveLogUploader {
+    public enum HttpVerb {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    public class Request {
+
+        public static TOUT JSON<TIN, TOUT>(string url, HttpVerb verb, TIN data) {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var serializer = new JsonSerializer();
+            request.ContentType = "application/json";
+            request.Method = verb.ToString();
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream())) {
+                using (var jsonWriter = new JsonTextWriter(streamWriter)) {
+                    serializer.Serialize(jsonWriter, data);
+                }
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            using (var streamReader = new StreamReader(response.GetResponseStream())) {
+                // TODO: JSON read
+            }
+            
+            return default(TOUT);
+        }
+
+    }
+}
--- DiveLogUploader/Writers/LittleLogDiveWriter.cs
+++ DiveLogUploader/Writers/LittleLogDiveWriter.cs
@@ -1,10 +1,13 @@
 ﻿using LibDiveComputer;
+using System.Net;
 
 namespace DiveLogUploader.Writers {
 
     public class LittleLogDiveWriter : AsyncDiveWriter {
-
+        
         public override void SetDevice(Device d) {
+            base.SetDevice(d);
+            
         }
 
         protected override void ProcessDive(Dive dive) {

55bd48c3b631a3c8bc7a897864e9b22af5afdcdc
Vincent Hagen <vinnie@script4web.nl>
2017-09-30 14:59:49 +0200

    Added rounding for tank pressure

--- divingserver/src/dives.ts
+++ divingserver/src/dives.ts
@@ -203,7 +203,7 @@ router.post("/", async (req, res) => {
 
     body.tanks = `{"${body.tanks.map((tank) => {
         // tslint:disable-next-line:max-line-length
-        return `(${tank.volume},${tank.oxygen},\\"(${tank.pressure.begin},${Math.round(tank.pressure.end)},${Math.round(tank.pressure.type)})\\")`;
+        return `(${tank.volume},${tank.oxygen},\\"(${Math.round(tank.pressure.begin)},${Math.round(tank.pressure.end)},${tank.pressure.type})\\")`;
     }).join('","')}"}`;
 
     body.user_id = userid;

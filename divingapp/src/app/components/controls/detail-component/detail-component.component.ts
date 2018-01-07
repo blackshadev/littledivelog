695bd4ecb8e343bda9321f6f9aee54f18b7d0439
Vincent Hagen <vinnie@script4web.nl>
2018-01-07 17:15:39 +0100

    WIP: first implementation of the detail component

--- divingapp/src/app/components/controls/detail-component/detail-component.component.ts
+++ divingapp/src/app/components/controls/detail-component/detail-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, TemplateRef, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,52 @@ import { FormGroup } from '@angular/forms';
     styleUrls: ['./detail-component.component.scss']
 })
 export class DetailComponentComponent implements OnInit {
+    @Input() data: any;
     @Input() form: FormGroup;
     @Output() onSubmit: EventEmitter<any> = new EventEmitter();
+    @Output() onBack: EventEmitter<any> = new EventEmitter();
+    // @Output() onReset: EventEmitter<any> = new EventEmitter();
+    // @Output() onReset: EventEmitter<any> = new EventEmitter();
 
-    constructor() { }
+    get pageKeys() { return Object.keys(this.pages); }
+    @Input() pages: { [name: string]: TemplateRef<any> } = {};
+    @Input() defaultPage;
+
+    @ViewChild('content') private content;
+
+    constructor(
+        private hostElement: ElementRef,
+    ) { }
 
     ngOnInit() {
+    }
+
+    reset() {
+        this.form.reset(this.data);
+    }
+
+    onEnter(e: KeyboardEvent) {
+
+        // prevent submit
+        e.preventDefault();
+
+        // tab on enter
+        const contentEl = this.content.nativeElement as HTMLElement;
+        const currentTab = contentEl.querySelector('.tab-pane.active');
+        if (currentTab && e.target instanceof HTMLInputElement) {
+            e.target.blur();
+            const all = currentTab.querySelectorAll('input.form-control') as NodeListOf<HTMLInputElement> ;
+            let iX: number;
+            for (iX = 0; iX < all.length; iX++) {
+                if (all.item(iX) === e.target) {
+                    break;
+                }
+            }
+            if (iX + 1 < all.length) {
+                all.item(iX + 1).select();
+            }
+        }
+
     }
 
 }

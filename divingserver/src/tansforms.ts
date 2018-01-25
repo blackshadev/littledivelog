a3dba041149d3d0358639ea7345e23de6da9559d
blackshadev <vinnie@script4web.nl>
2018-01-25 22:49:56 +0100

    Rounded tank values

--- divingserver/src/tansforms.ts
+++ divingserver/src/tansforms.ts
@@ -1,21 +1,30 @@
-
 export interface ITank {
     volume: number;
     oxygen: number;
     pressure: {
         begin: number;
         end: number;
-        type: "bar"|"psi";
+        type: "bar" | "psi";
     };
 }
 
 export function tanksJSONtoType(tanks: ITank[]): string {
-    return `{"${tanks.map((tank) => {
-        // tslint:disable-next-line:max-line-length
-        return `(${isNull(tank.volume)},${isNull(tank.oxygen)},\\"(${isNull(tank.pressure.begin)},${isNull(tank.pressure.end)},${isNull(tank.pressure.type)})\\")`;
-    }).join('","')}"}`;
+    return `{"${tanks
+        .map(tank => {
+            // tslint:disable-next-line:max-line-length
+            return `(${isNullOrRound(tank.volume)},${isNullOrRound(
+                tank.oxygen,
+            )},\\"(${isNullOrRound(tank.pressure.begin)},${isNullOrRound(
+                tank.pressure.end,
+            )},${isNullOrRound(tank.pressure.type)})\\")`;
+        })
+        .join('","')}"}`;
 }
 
-function isNull(v: any): string {
+function isNull(v: any): any {
     return v === null || v === undefined ? "" : v;
+}
+
+function isNullOrRound(v: any): any {
+    return v === null || v === undefined ? "" : Math.round(v);
 }

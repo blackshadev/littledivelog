ffff148702909a500a756f7ebf2de8c8d3ff9f48
Vincent Hagen <vinnie@script4web.nl>
2018-01-27 13:45:00 +0100

    Updated ecosystem

--- divingapp/ecosystem.config.js
+++ divingapp/ecosystem.config.js
@@ -4,15 +4,14 @@ module.exports = {
      * http://pm2.keymetrics.io/docs/usage/application-declaration/
      */
     apps: [
-
         // First application
         {
             name: 'divingapp',
             script: 'app-server/build/app.js',
             env: {
-                "PORT": 8080
-            }
-        }
+                PORT: 8080,
+            },
+        },
     ],
 
     /**
@@ -21,13 +20,14 @@ module.exports = {
      */
     deploy: {
         prod: {
-            key: "~/.ssh/dive_app_key",
+            key: '~/.ssh/dive_app_key',
             user: 'dive.littledev.nl',
             host: 'mira.littledev.nl',
             ref: 'origin/master',
             repo: 'git@git.littledev.nl:blackshade/divecomputer.git',
             path: '/home/dive.littledev.nl/',
-            'post-deploy': 'cd divingapp &&yarn && ./node_modules/.bin/ng build --aot --prod && cd app-server && yarn && ./node_modules/.bin/tsc && cd .. && pm2 reload ecosystem.config.js'
-        }
-    }
+            'post-deploy':
+                'cd divingapp && yarn && sed -i \'/"last 1 major version",/d\' ./node_modules/bootstrap/package.json && sed -i \'/">= 1%",/d\' ./node_modules/bootstrap/package.json && ./node_modules/.bin/ng build --aot --prod && cd app-server && yarn && ./node_modules/.bin/tsc && cd .. && pm2 reload ecosystem.config.js',
+        },
+    },
 };

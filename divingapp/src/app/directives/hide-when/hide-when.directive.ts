87ec696a071c3871cee31b5a17fe9e9724420721
Vincent Hagen <vinnie@script4web.nl>
2020-04-26 15:33:54 +0200

    removed cancel button for mobile.

--- divingapp/src/app/directives/hide-when/hide-when.directive.ts
+++ divingapp/src/app/directives/hide-when/hide-when.directive.ts
@@ -2,29 +2,28 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 type TSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-
 @Directive({
-    selector: '[appHideWhenMobile]'
+    selector: '[appHideWhenMobile]',
 })
 export class HideWhenMobileDirective {
+    private _condition: boolean;
+    private _size: TSize = 'md';
 
     @Input('appHideWhenMobile') set condition(v: boolean) {
         this._condition = v;
         this.apply();
     }
-    private _condition: boolean;
 
     @Input('isMobileWhen') set screenSize(s: TSize) {
         this._size = s;
         this.apply();
     }
-    private _size: TSize = 'md';
 
     get isActive(): boolean {
         return this._condition && this.checkSize();
     }
 
-    constructor(private el: ElementRef) { }
+    constructor(private el: ElementRef) {}
 
     @HostListener('window:resize', ['$event'])
     public onResize(event) {
@@ -34,61 +33,71 @@ export class HideWhenMobileDirective {
     private checkSize(): boolean {
         const w = window.innerWidth;
         switch (this._size) {
-            case 'xs': return w < 576;
-            case 'sm': return w < 768;
-            case 'md': return w < 992;
-            case 'lg': return w < 1200;
-            case 'xl': return true;
+            case 'xs':
+                return w < 576;
+            case 'sm':
+                return w < 768;
+            case 'md':
+                return w < 992;
+            case 'lg':
+                return w < 1200;
+            case 'xl':
+                return true;
         }
     }
 
     private apply() {
-        (<HTMLElement>this.el.nativeElement).style.display = this.isActive ? 'none' : '';
+        (<HTMLElement>this.el.nativeElement).style.display = this.isActive
+            ? 'none'
+            : '';
     }
-
-
 }
 
 @Directive({
-    selector: '[appHideWhenDesktop]'
+    selector: '[appHideWhenDesktop]',
 })
 export class HideWhenDesktopDirective {
+    private _condition: boolean;
+    private _size: TSize = 'sm';
 
     @Input('appHideWhenDesktop') set condition(v: boolean) {
         this._condition = v;
         this.apply();
     }
-    private _condition: boolean;
 
     @Input('isMobileWhen') set screenSize(s: TSize) {
         this._size = s;
         this.apply();
     }
-    private _size: TSize = 'sm';
 
     get isActive(): boolean {
         return this._condition && this.checkSize();
     }
 
-    private checkSize(): boolean {
-        const w = window.innerWidth;
-        switch (this._size) {
-            case 'xs': return w >= 768;
-            case 'sm': return w >= 992;
-            case 'md': return w >= 1200;
-            case 'lg': return true;
-        }
-    }
-
-    constructor(private el: ElementRef) { }
-
-    private apply() {
-        (<HTMLElement>this.el.nativeElement).style.display = this.isActive ? 'none' : '';
-    }
+    constructor(private el: ElementRef) {}
 
     @HostListener('window:resize', ['$event'])
     onResize(event) {
         this.apply();
     }
 
+    private checkSize(): boolean {
+        const w = window.innerWidth;
+        switch (this._size) {
+            case 'xs':
+                return w >= 768;
+            case 'sm':
+                return w >= 992;
+            case 'md':
+                return w >= 1200;
+            case 'lg':
+                return true;
+        }
+    }
+
+    private apply() {
+        (<HTMLElement>this.el.nativeElement).style.display = this.isActive
+            ? 'none'
+            : '';
+    }
 }

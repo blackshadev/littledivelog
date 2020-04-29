07e0c5f3a47ade16db3c8723defc871394c88ea8
Vincent Hagen <vinnie@script4web.nl>
2020-04-29 20:19:46 +0200

    fixing deploy script

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find divingapp/dist -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://littledev.nl/{} \\;"
+find divingapp/dist -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://littledev.nl/{} \;

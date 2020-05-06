31b9f8875ef7ea892f3b45480d2728ebe0d1ff05
vincent <vinnie@script4web.nl>
2020-05-06 19:43:58 +0200

    more test fixing

--- divingapp/src/app/components/dives/dive-profile/dive-profile.component.spec.ts
+++ divingapp/src/app/components/dives/dive-profile/dive-profile.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DiveProfileComponent } from './dive-profile.component';
+import { DiveService } from 'app/services/dive.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TagService } from 'app/services/tag.service';
+import { BuddyService } from 'app/services/buddy.service';
+import { DivetimePipe } from 'app/pipes/divetime.pipe';
 
 describe('DiveProfileComponent', () => {
     let component: DiveProfileComponent;
@@ -8,7 +13,10 @@ describe('DiveProfileComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [DiveProfileComponent]
+            imports: [HttpClientTestingModule],
+            declarations: [DiveProfileComponent, DivetimePipe],
+            providers: [DiveService, BuddyService, TagService],
+            
         }).compileComponents();
     }));
 

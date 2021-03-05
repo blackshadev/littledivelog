d766b3e3a5550237111b1f67b9290fa7dce393a8
Vincent Hagen <vinnie@script4web.nl>
2021-03-05 20:05:21 +0100

    Working on tests

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
         customLaunchers: {
             chromeTravisCi: {
                 base: 'ChromeHeadless',
-                flags: ['--no-sandbox'],
+                flags: ['--no-sandbox', '--disable-gpu'],
             },
         },
         reporters:

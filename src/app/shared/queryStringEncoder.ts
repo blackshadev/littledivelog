15ece8993bb1c7c4a8ae3860cebf6b08b5371a70
Vincent Hagen <vinnie@script4web.nl>
2021-06-02 21:20:46 +0200

    Fix searching for buddies and tags

---
+++ src/app/shared/queryStringEncoder.ts
@@ -0,0 +1,32 @@
+export function encodeAsQueryString(params: {
+  [key: string]: unknown;
+}): string {
+  return Object.keys(params)
+    .map((k) => encodeAsQueryComponent(k, params[k]))
+    .join("&");
+}
+
+function isNativeValue(value: unknown): value is number | string | boolean {
+  return (
+    typeof value === "string" ||
+    typeof value === "number" ||
+    typeof value === "boolean"
+  );
+}
+
+function encodeAsQueryComponent(key: string, value: unknown): string {
+  if (isNativeValue(value)) {
+    return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+  }
+
+  if (typeof value === "object" && Array.isArray(value)) {
+    return value
+      .map(
+        (element) =>
+          `${encodeURIComponent(key)}[]=${encodeURIComponent(element)}`
+      )
+      .join("&");
+  }
+
+  throw new Error("Unsupported type for query encoding");
+}
